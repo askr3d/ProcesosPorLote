ca825af02143f1e21943758b21800c2d1eb5eb2f
Giovanni Cord <asesino9393@gmail.com>
2024-08-22 23:33:19 -0600

    Agregar archivos de proyecto.

---
+++ ProcesosPorLote/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace ProcesosPorLote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumProcesos = new System.Windows.Forms.TextBox();
+            this.timerGlobal = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGenerarProcesos = new System.Windows.Forms.Button();
+            this.lblValidacionNumProcesos = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.listEspera = new System.Windows.Forms.ListView();
+            this.Lote = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Operacion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Tiempo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listEjecucion = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listTerminados = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnObtenerResultados = new System.Windows.Forms.Button();
+            this.lblReporteMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No. Procesos";
+            // 
+            // txtNumProcesos
+            // 
+            this.txtNumProcesos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumProcesos.Location = new System.Drawing.Point(183, 47);
+            this.txtNumProcesos.Name = "txtNumProcesos";
+            this.txtNumProcesos.Size = new System.Drawing.Size(118, 26);
+            this.txtNumProcesos.TabIndex = 1;
+            this.txtNumProcesos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timerGlobal
+            // 
+            this.timerGlobal.Interval = 1000;
+            this.timerGlobal.Tick += new System.EventHandler(this.timerGlobal_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(97, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "En Espera";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(479, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "En Ejecucion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(882, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Terminados";
+            // 
+            // btnGenerarProcesos
+            // 
+            this.btnGenerarProcesos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(165)))), ((int)(((byte)(233)))));
+            this.btnGenerarProcesos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGenerarProcesos.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnGenerarProcesos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerarProcesos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarProcesos.ForeColor = System.Drawing.Color.White;
+            this.btnGenerarProcesos.Location = new System.Drawing.Point(317, 39);
+            this.btnGenerarProcesos.Name = "btnGenerarProcesos";
+            this.btnGenerarProcesos.Size = new System.Drawing.Size(98, 43);
+            this.btnGenerarProcesos.TabIndex = 4;
+            this.btnGenerarProcesos.Text = "Generar";
+            this.btnGenerarProcesos.UseVisualStyleBackColor = false;
+            this.btnGenerarProcesos.Click += new System.EventHandler(this.btnGenerarProcesos_Click);
+            // 
+            // lblValidacionNumProcesos
+            // 
+            this.lblValidacionNumProcesos.AutoSize = true;
+            this.lblValidacionNumProcesos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblValidacionNumProcesos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValidacionNumProcesos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.lblValidacionNumProcesos.Location = new System.Drawing.Point(179, 108);
+            this.lblValidacionNumProcesos.Name = "lblValidacionNumProcesos";
+            this.lblValidacionNumProcesos.Size = new System.Drawing.Size(103, 20);
+            this.lblValidacionNumProcesos.TabIndex = 5;
+            this.lblValidacionNumProcesos.Text = "mensaje aqui";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(772, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 24);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Tiempo: ";
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTiempo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblTiempo.Location = new System.Drawing.Point(871, 53);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(43, 20);
+            this.lblTiempo.TabIndex = 0;
+            this.lblTiempo.Text = "segs";
+            // 
+            // listEspera
+            // 
+            this.listEspera.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Lote,
+            this.Nombre,
+            this.Operacion,
+            this.Tiempo});
+            this.listEspera.HideSelection = false;
+            this.listEspera.Location = new System.Drawing.Point(23, 191);
+            this.listEspera.Name = "listEspera";
+            this.listEspera.Size = new System.Drawing.Size(316, 380);
+            this.listEspera.TabIndex = 6;
+            this.listEspera.UseCompatibleStateImageBehavior = false;
+            this.listEspera.View = System.Windows.Forms.View.Details;
+            // 
+            // Lote
+            // 
+            this.Lote.Tag = "";
+            this.Lote.Text = "Lote";
+            // 
+            // Nombre
+            // 
+            this.Nombre.Text = "Nombre";
+            this.Nombre.Width = 94;
+            // 
+            // Operacion
+            // 
+            this.Operacion.Text = "Operacion";
+            this.Operacion.Width = 80;
+            // 
+            // Tiempo
+            // 
+            this.Tiempo.Text = "Tiempo";
+            this.Tiempo.Width = 77;
+            // 
+            // listEjecucion
+            // 
+            this.listEjecucion.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listEjecucion.HideSelection = false;
+            this.listEjecucion.Location = new System.Drawing.Point(380, 331);
+            this.listEjecucion.Name = "listEjecucion";
+            this.listEjecucion.Size = new System.Drawing.Size(321, 146);
+            this.listEjecucion.TabIndex = 6;
+            this.listEjecucion.UseCompatibleStateImageBehavior = false;
+            this.listEjecucion.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Tag = "";
+            this.columnHeader1.Text = "Lote";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nombre";
+            this.columnHeader2.Width = 94;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Operacion";
+            this.columnHeader3.Width = 80;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Tiempo";
+            this.columnHeader4.Width = 77;
+            // 
+            // listTerminados
+            // 
+            this.listTerminados.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listTerminados.HideSelection = false;
+            this.listTerminados.Location = new System.Drawing.Point(776, 193);
+            this.listTerminados.Name = "listTerminados";
+            this.listTerminados.Size = new System.Drawing.Size(316, 380);
+            this.listTerminados.TabIndex = 6;
+            this.listTerminados.UseCompatibleStateImageBehavior = false;
+            this.listTerminados.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Tag = "";
+            this.columnHeader5.Text = "Lote";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Nombre";
+            this.columnHeader6.Width = 94;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Operacion";
+            this.columnHeader7.Width = 80;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Tiempo";
+            this.columnHeader8.Width = 77;
+            // 
+            // btnObtenerResultados
+            // 
+            this.btnObtenerResultados.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(179)))), ((int)(((byte)(8)))));
+            this.btnObtenerResultados.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnObtenerResultados.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnObtenerResultados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnObtenerResultados.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnObtenerResultados.ForeColor = System.Drawing.Color.White;
+            this.btnObtenerResultados.Location = new System.Drawing.Point(776, 599);
+            this.btnObtenerResultados.Name = "btnObtenerResultados";
+            this.btnObtenerResultados.Size = new System.Drawing.Size(316, 43);
+            this.btnObtenerResultados.TabIndex = 4;
+            this.btnObtenerResultados.Text = "Obtener Resultados";
+            this.btnObtenerResultados.UseVisualStyleBackColor = false;
+            this.btnObtenerResultados.Click += new System.EventHandler(this.btnObtenerResultados_Click);
+            // 
+            // lblReporteMsg
+            // 
+            this.lblReporteMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblReporteMsg.AutoSize = true;
+            this.lblReporteMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReporteMsg.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lblReporteMsg.Location = new System.Drawing.Point(20, 665);
+            this.lblReporteMsg.Name = "lblReporteMsg";
+            this.lblReporteMsg.Size = new System.Drawing.Size(121, 16);
+            this.lblReporteMsg.TabIndex = 0;
+            this.lblReporteMsg.Text = "Mensaje Exitoso";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1152, 704);
+            this.Controls.Add(this.listEjecucion);
+            this.Controls.Add(this.listTerminados);
+            this.Controls.Add(this.listEspera);
+            this.Controls.Add(this.lblValidacionNumProcesos);
+            this.Controls.Add(this.btnObtenerResultados);
+            this.Controls.Add(this.btnGenerarProcesos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumProcesos);
+            this.Controls.Add(this.lblReporteMsg);
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Procesamiento Por Lotes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumProcesos;
+        private System.Windows.Forms.Timer timerGlobal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGenerarProcesos;
+        private System.Windows.Forms.Label lblValidacionNumProcesos;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.ListView listEspera;
+        private System.Windows.Forms.ColumnHeader Lote;
+        private System.Windows.Forms.ColumnHeader Nombre;
+        private System.Windows.Forms.ColumnHeader Operacion;
+        private System.Windows.Forms.ColumnHeader Tiempo;
+        private System.Windows.Forms.ListView listEjecucion;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ListView listTerminados;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.Button btnObtenerResultados;
+        private System.Windows.Forms.Label lblReporteMsg;
+    }
+}
+
---
+++ ProcesosPorLote/Form1.cs
@@ -0,0 +1,250 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ProcesosPorLote
+{
+    public partial class Form1 : Form
+    {
+        private int contadorGlobal = 0;
+        private List<String> nombres = new List<String> { "Carlos", "Juan", "Luis" };
+        private List<String> signos = new List<String>{"+", "-", "*", "/"};
+        private int numLote = 1;
+        private int numProcesos = 0;
+        private List<Proceso> procesos = new List<Proceso>();
+        private List<Proceso> procesosTerminados = new List<Proceso>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblValidacionNumProcesos.Text = "";
+            lblTiempo.Text = contadorGlobal + " segs";
+            btnObtenerResultados.Enabled = false;
+            lblReporteMsg.Text = "";
+        }
+
+        private void btnGenerarProcesos_Click(object sender, EventArgs e)
+        {
+            lblValidacionNumProcesos.Text = "";
+            if(txtNumProcesos.Text.Length == 0 || !int.TryParse(txtNumProcesos.Text, out _))
+            {
+                lblValidacionNumProcesos.Text = "Debe intruducir un numero";
+                return;
+            }
+            btnObtenerResultados.Enabled = false;
+            numProcesos = int.Parse(txtNumProcesos.Text);
+            lblTiempo.Text = contadorGlobal + " segs";
+            lblReporteMsg.Text = "";
+            empezarCronometro();
+            ingresarProcesos();
+            numLote++;
+        }
+        private void ingresarProcesos()
+        {
+            for (int i = 0; i < numProcesos; i++)
+            {
+                generarProceso(numLote);
+            }
+        }
+
+        private void generarProceso(int id)
+        {
+            Random rnd = new Random();
+            Proceso proceso = new Proceso
+            {
+                id = id,
+                nombre= nombres[rnd.Next(0,nombres.Count)],
+                signo= signos[rnd.Next(0, signos.Count)],
+                numFinal = rnd.Next(0, 10),
+                numInicio= rnd.Next(0, 10),
+                tiempo= rnd.Next(1, 4),
+            };
+            proceso.tiempoOriginal = proceso.tiempo;
+
+
+            procesos.Add(proceso);
+
+            ListViewItem item = crearListaElemento(proceso);
+
+            listEspera.Items.Add(item);
+        }
+
+        private void ejecutarProceso()
+        {
+            Proceso procesoActual = procesos.FirstOrDefault();
+            listEjecucion.Items.Clear();
+            if (procesoActual != null)
+            {
+                if (!procesoActual.ejecucion)
+                {
+                    procesoActual.ejecucion = true;
+                    listEspera.Items.RemoveAt(0);
+                }
+                ListViewItem item = crearListaElemento(procesoActual);
+                if (procesoActual.tiempo > 0)
+                {
+                    procesoActual.tiempo--;
+                    listEjecucion.Items.Add(item);
+                }
+                else
+                {
+                    procesoActual.tiempo = procesoActual.tiempoOriginal;
+                    listTerminados.Items.Add(item);
+                    procesosTerminados.Add(procesoActual);
+                    procesos.Remove(procesoActual);
+
+                    Proceso procesoSiguiente = procesos.FirstOrDefault();
+                    if(procesoSiguiente != null)
+                    {
+                        if (!procesoSiguiente.ejecucion)
+                        {
+                            procesoSiguiente.ejecucion = true;
+                            listEspera.Items.RemoveAt(0);
+                        }
+                        contadorGlobal--;
+                        ListViewItem itemSiguiente = crearListaElemento(procesoSiguiente);
+                        listEjecucion.Items.Add(itemSiguiente);
+                    }
+                    else
+                    {
+                        timerGlobal.Stop();
+                        btnObtenerResultados.Enabled = true;
+                    }
+                }
+            }
+            else
+            {
+                timerGlobal.Stop();
+                btnObtenerResultados.Enabled = true;
+            }
+        }
+
+        public ListViewItem crearListaElemento(Proceso proceso)
+        {
+            ListViewItem item = new ListViewItem(proceso.id.ToString());
+            item.SubItems.Add(proceso.nombre);
+            if (proceso.tiempo == 0)
+            {
+                item.SubItems.Add(
+                    proceso.numInicio + proceso.signo + proceso.numFinal + " = " +
+                    calcularOperacion(proceso.numInicio, proceso.numFinal, proceso.signo).ToString());
+                item.SubItems.Add(proceso.tiempoOriginal.ToString());
+            }
+            else
+            {
+                item.SubItems.Add(proceso.numInicio + proceso.signo + proceso.numFinal);
+                item.SubItems.Add(proceso.tiempo.ToString());
+            }
+
+            return item;
+        }
+
+        public int calcularOperacion(int num1, int num2, String signo)
+        {
+            int resultado = 0;
+            switch (signo)
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                default:
+                    break;
+            }
+
+            return resultado;
+        }
+
+
+        //Cronometro Logica
+        private void empezarCronometro()
+        {
+            timerGlobal.Start();
+        }
+
+        private void sumarSegundos()
+        {
+            lblTiempo.Text = contadorGlobal++ + " segs";
+            ejecutarProceso();
+        }
+
+        private void timerGlobal_Tick(object sender, EventArgs e)
+        {
+            sumarSegundos();
+        }
+
+        private void generarReporteExcel()
+        {
+
+            string filePath = "./resultado.xlsx";
+
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Hoja1");
+
+                worksheet.Cells[1, 1].Value = "Lote";
+                worksheet.Cells[1, 2].Value = "Nombre";
+                worksheet.Cells[1, 3].Value = "Operacion";
+                worksheet.Cells[1, 4].Value = "Tiempo";
+                int celda = 2;
+
+                procesosTerminados.ForEach(procesoAux =>
+                {
+                    worksheet.Cells[celda, 1].Value = procesoAux.id;
+                    worksheet.Cells[celda, 2].Value = procesoAux.nombre;
+                    worksheet.Cells[celda, 3].Value = procesoAux.numInicio + procesoAux.signo + procesoAux.numFinal + " = " +
+                    calcularOperacion(procesoAux.numInicio, procesoAux.numFinal, procesoAux.signo).ToString();
+                    worksheet.Cells[celda, 4].Value = procesoAux.tiempoOriginal;
+                    celda++;
+                });
+
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+                FileInfo excelFile = new FileInfo(filePath);
+                package.SaveAs(excelFile);
+            }
+            lblReporteMsg.Text = "Archivo Creado en: " + Path.GetFullPath(filePath);
+        }
+
+        private void btnObtenerResultados_Click(object sender, EventArgs e)
+        {
+            generarReporteExcel();
+            procesosTerminados.Clear();
+            listTerminados.Items.Clear();
+            numLote = 1;
+        }
+    }
+
+    public class Proceso
+    {
+        public int id;
+        public String nombre;
+        public int numInicio;
+        public int numFinal;
+        public String signo;
+        public int tiempo;
+        public int tiempoOriginal;
+        public bool ejecucion = false;
+    }
+}
